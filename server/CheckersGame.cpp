4187a83fe0e27c02e7d97508bc29ffbfafbf83c2
Dmitrii Onshin <dmitrii.onshin@mail.ru>
2025-03-26 16:31:20 +0300

    fix resetBoard

--- server/CheckersGame.cpp
+++ server/CheckersGame.cpp
@@ -8,18 +8,16 @@ void CheckersGame::resetBoard() {
     board.assign(8, std::vector(8, '.'));
     currentPlayer = 'W';
 
-    // for (int i = 0; i < 3; ++i) {
-    //     for (int j = (i + 1) % 2; j < 8; j += 2) {
-    //         board[i][j] = 'B';
-    //     }
-    // }
+    for (int i = 0; i < 3; ++i) {
+        for (int j = (i + 1) % 2; j < 8; j += 2) {
+            board[i][j] = 'B';
+        }
+    }
     for (int i = 5; i < 8; ++i) {
         for (int j = (i + 1) % 2; j < 8; j += 2) {
             board[i][j] = 'W';
         }
     }
-
-    board[4][1] = 'B';
 }
 
 void CheckersGame::displayBoard() {
