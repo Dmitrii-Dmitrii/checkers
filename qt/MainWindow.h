b0a91cbb4758f7c05427ea55f47169154e3d2349
Dmitrii Onshin <dmitrii.onshin@mail.ru>
2025-05-04 20:47:54 +0300

    almost done qt

--- qt/MainWindow.h
+++ qt/MainWindow.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <QWidget>
 #include <QLineEdit>
 #include <QPushButton>
@@ -9,21 +7,32 @@
 #include "BoardWidget.h"
 #include "CheckersClient.h"
 
+
 class MainWindow : public QWidget {
     Q_OBJECT
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
-    private slots:
-        void onSendClicked();
+private slots:
+    void onSendClicked();
+
     void onReceived(const QString &msg);
 
+    void resetGame();
+
 private:
+    void checkWinner(int whiteCount, int blackCount);
+
+    void displayWinner(const QString &color);
+
     QLineEdit *m_moveEdit;
     QPushButton *m_sendBtn;
     BoardWidget *m_boardWidget;
     QTextEdit *m_log;
     std::unique_ptr<CheckersClient> m_client;
     size_t m_lastMessageIndex;
+    bool m_gameOver;
 };
